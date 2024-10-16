72fd0935d5ce6070fc24f4db5ec0dbcfec1ee082
Froddes <froddes.dev@gmail.com>
2024-10-16 18:32:37 +0200

    update gitpages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        index: 'index.html'
-      },
-    },
-  },
+  plugins: [react()],
+  base: "/FroddesInfo/"
 })
