f7612c370656ad56da74345e5c94ea3e779d95e5
Froddes <froddes.dev@gmail.com>
2024-10-16 18:21:33 +0200

    linux

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/FroddesInfo/"
+  base: "/FroddesInfo/",
+  build: {
+    rollupOptions: {
+      input: {
+        index: 'index.html',
+        ...inputEntries,
+      },
+    },
+  },
 })
